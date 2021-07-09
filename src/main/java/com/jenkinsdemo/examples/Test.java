efb37ce409e9d97505267adf1c014f8e432bedf5
lakshrs <laksh_rs@yahoo.co.in>
2021-07-09 14:05:02 +0530

    second commit

--- src/main/java/com/jenkinsdemo/examples/Test.java
+++ src/main/java/com/jenkinsdemo/examples/Test.java
@@ -3,5 +3,6 @@ package com.jenkinsdemo.examples;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Jenkins test");
+        System.out.println("Second commit");
     }
 }
